e0fb2dbf11352808cbdb29dafabe674b73b7658d
Kyle Evardome <main.kyledaniel.evardome@cvsu.edu.ph>
2025-01-19 20:19:24 +0800

    fixed directory structure, added appointment logic backend (wip), removed sensitive service account key, recovered deleted files LOL

--- server/src/firebase.ts
+++ server/src/firebase.ts
@@ -1,10 +1,18 @@
-import admin from "firebase-admin";
+import * as admin from "firebase-admin";
 import { getFirestore } from "firebase-admin/firestore";
+import * as dotenv from "dotenv";
 
-const serviceAccount = require("../service-account.json");
+dotenv.config();
 
+const serviceAccountPath = process.env.GOOGLE_CLOUD_KEY_FILE;
+
+if (!serviceAccountPath) {
+  throw new Error("Environment variable GOOGLE_CLOUD_KEY_FILE is not defined.");
+}
+
+// Initialize Firebase Admin SDK
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(require(serviceAccountPath)),
 });
 
 const db = getFirestore();
