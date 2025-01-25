500bc4b077f0b4290f4622fbc8300624c05060e2
Louie <christianlouiecorpuz@gmail.com>
2025-01-25 12:23:59 +0800

    Fix bug

--- server/src/firebase/index.ts
+++ server/src/firebase/index.ts
@@ -1,9 +1,14 @@
 import { initializeApp, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
+import { getAuth } from 'firebase-admin/auth';
+
 import * as serviceAccount from '../../service-account.json';
 
 initializeApp({
   credential: cert(serviceAccount as any)
 });
 
-export const db = getFirestore();
+const auth = getAuth();
+const db = getFirestore();
+
+export { auth, db };
--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { auth } from '../firebase';
+import { auth } from '../firebase/index'; 
 
 export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
   try {
